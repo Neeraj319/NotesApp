e552492857c73a7686ac08c8f5491eb7f993bd5f
Neeraj <itskop520@gmail.com>
2020-12-23 09:37:20 +0545

    admin pannel changes made

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,2 @@
 from django.contrib import admin
 from .models import Notes
-from django_summernote.admin import SummernoteModelAdmin
-
-
-@admin.register(Notes)
-class NotesAdmin(SummernoteModelAdmin):
-    list_display = ['title', ]
-    summernote_fields = ['content', ]
